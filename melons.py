7658e9fbbf482c800845f5edca40b8ed754a43e0
Hackbright Student <info@hackbrightacademy.com>
2016-07-15 13:03:35 -0700

    not complete - fix international melon order

--- melons.py
+++ melons.py
@@ -2,68 +2,49 @@
 
 #establish parent class
 class AbstractMelonOrder(object):
+    shipped = False
     #parent method to be called initiating the program and accepting the arguements
-    def __init__(self, species, qty, shipped, order_type, tax):
+    def __init__(self, species, qty, order_type, tax, country_code, base_price=5):
     #defining the parent attributes and methods to be accessible to the child class
         self.species = species
         self.qty = qty
-        self.shipped = False
         self.order_type = order_type
         self.tax = tax
+        self.base_price = base_price
+        self.country_code = country_code
         self.get_total()
         self.marked_shipped()
 
 
-    def get_total(self, tax, qty, base_price = 5):
-        print base_price
-        if self.species == "Christmas":
-            base_price = base_price * 1.5
-            print "I'm a xmas melon", base_price
-        total = (1 + self.tax) * self.qty * base_price
+    def get_total(self):
+        # if self.species == "Christmas":
+        #     base_price = base_price * 1.5
+        #     print "I'm a xmas melon", base_price
+        total = (1 + self.tax) * self.qty * self.base_price
         return total
 
-    def marked_shipped(self, shipped):
+
+    def marked_shipped(self):
         self.shipped = True
-        print self.shipped
+        print self.shipped, "I'm shipped"
+
 
 
 
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
-
-    def __init__(self, order_type, tax):
-        """Initialize melon order attributes"""
-        super(DomesticMelonOrder, self).__init__("domestic", 0.08)
-
-    def get_total(self):
-        """Calculate price."""
-        return super(DomesticMelonOrder, self).get_total(0.08)
-
-    def mark_shipped(self):
-        """Set shipped to true."""
-        return super(DomesticMelonOrder, self).marked_shipped(True)
+    tax = 0.08
+    order_type = "Domestic"
+    country_code = "US"
 
 
-
-
-class InternationalMelonOrder(AbstractMelonOrder):
+class InternationalMelonOrder(AbstractMelonOrder, country_code):
     """An international (non-US) melon order."""
-    self.country_code = country_code
+    tax = 0.12
+    order_type = "International"
     self.get_country_code()
-
-    def __init__(self, order_type, country_code, tax):
-        """Initialize melon order attributes"""
-        super(InternationalMelonOrder, self).__init__("international", country_code, 0.12)
-
-    def get_total(self):
-        """Calculate price."""
-        return super(InternationalMelonOrder, self).get_total(0.12)
-
-    def mark_shipped(self):
-        """Set shipped to true."""
-        return super(InternationalMelonOrder, self).mark_shipped(True)
-
-    def get_country_code(self, country_code):
+    
+    def get_country_code(self):
         """Return the country code."""
         return self.country_code
